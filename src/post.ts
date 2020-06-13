a188e7cd61322abbfa4410c3c3345fbe3f5f532b
Paul Merlin <paul@nosphere.org>
2020-06-13 13:36:11 +0200

    Debug logging

--- src/post.ts
+++ src/post.ts
@@ -2,7 +2,7 @@ import * as core from "@actions/core";
 
 // Invoked by GitHub Actions
 export async function run() {
-    core.debug("POST Gradle Command Action")
+    core.info("POST Gradle Command Action")
 }
 
 run();
