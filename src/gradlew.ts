76a33e31af59ca9635ae3793050c161f9b947d38
Daz DeBoer <daz@gradle.com>
2021-06-24 12:13:54 -0700

    Validate presense of gradle wrapper to provide better feedback
    - Provide a more useful error message when no Gradle wrapper can be located,
      and 'gradle-version' or 'gradle-executable' is not used.
    - Add test for case where wrapper is missing.
      This isn't really a "test" per-se, but this failing build invocation makes it
      easy to verify the GitHub action behaviour when the build is misconfigured.

--- src/gradlew.ts
+++ src/gradlew.ts
@@ -1,3 +1,7 @@
+import * as path from 'path'
+import fs from 'fs'
+import * as core from '@actions/core'
+
 const IS_WINDOWS = process.platform === 'win32'
 
 export function wrapperFilename(): string {
@@ -6,4 +10,19 @@ export function wrapperFilename(): string {
 
 export function installScriptFilename(): string {
     return IS_WINDOWS ? 'gradle.bat' : 'gradle'
+}
+
+export function validateGradleWrapper(gradlewDirectory: string): void {
+    const wrapperProperties = path.resolve(
+        gradlewDirectory,
+        'gradle/wrapper/gradle-wrapper.properties'
+    )
+    if (!fs.existsSync(wrapperProperties)) {
+        core.warning(
+            `Cannot locate a Gradle wrapper properties file at '${wrapperProperties}'. Specify 'gradle-version' or 'gradle-executable' for projects without Gradle wrapper configured.`
+        )
+        throw new Error(
+            `Cannot locate a Gradle wrapper within '${gradlewDirectory}'`
+        )
+    }
 }
