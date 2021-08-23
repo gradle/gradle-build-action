e5310e0c754621f7de0fb39109afd417c3d3e9b5
Daz DeBoer <daz@gradle.com>
2021-08-23 14:33:49 -0600

    Truncate args when added to cache key - Remove excess whitespace - Truncate to 400 characters

--- __tests__/cache-utils.test.ts
+++ __tests__/cache-utils.test.ts
@@ -1,10 +1,10 @@
-import * as cryptoUtils from '../src/crypto-utils'
+import * as cacheUtils from '../src/cache-utils'
 import * as path from 'path'
 
-describe('crypto-utils', () => {
+describe('cacheUtils-utils', () => {
     describe('can hash', () => {
         it('a directory', async () => {
-            const hash = await cryptoUtils.hashFiles(
+            const hash = await cacheUtils.hashFiles(
                 path.resolve('__tests__/data/crypto-utils-test/gradle')
             )
             expect(hash).toBe(
@@ -14,7 +14,7 @@ describe('crypto-utils', () => {
             )
         })
         it('a directory with a glob', async () => {
-            const hash = await cryptoUtils.hashFiles(
+            const hash = await cacheUtils.hashFiles(
                 path.resolve('__tests__/data/crypto-utils-test/'),
                 ['gradle/**']
             )
@@ -25,7 +25,7 @@ describe('crypto-utils', () => {
             )
         })
         it('a directory with globs', async () => {
-            const hash = await cryptoUtils.hashFiles(
+            const hash = await cacheUtils.hashFiles(
                 path.resolve('__tests__/data/crypto-utils-test/'),
                 ['**/*.gradle', 'gradle/**']
             )
@@ -34,6 +34,32 @@ describe('crypto-utils', () => {
                     ? 'd9b66fded38f79f601ce745d64ed726a8df8c0b242b02bcd2c1d331f54742ad6'
                     : 'f42cd10636f09799f4e01cc84e7ae906cc1d9140f1446f8dcd054d19cbc44c2b'
             )
+        })
+    })
+    describe('can truncate args', () => {
+        test('handles zero-length string', () => {
+            expect(cacheUtils.truncateArgs('')).toBe('')
+        })
+        test('leaves short string untouched', () => {
+            expect(
+                cacheUtils.truncateArgs('short string that-should-be-untouched')
+            ).toBe('short string that-should-be-untouched')
+        })
+        test('truncates long string', () => {
+            const longString = 'a'.repeat(500)
+            expect(cacheUtils.truncateArgs(longString)).toBe('a'.repeat(400))
+        })
+        test('trims leading and trailing whitespace', () => {
+            expect(cacheUtils.truncateArgs('    this is an arg      ')).toBe(
+                'this is an arg'
+            )
+        })
+        test('removes repeated whitespace', () => {
+            expect(
+                cacheUtils.truncateArgs(
+                    '   this     one     has long   \t\n\t\r  spaces    '
+                )
+            ).toBe('this one has long spaces')
         })
     })
 })
