d9cc0aeccfe2c0310b83da575338ecac8f24b414
Daz DeBoer <daz@gradle.com>
2021-08-22 20:28:46 -0600

    Remove old caching code

--- __tests__/cache-utils.test.ts
+++ __tests__/cache-utils.test.ts
@@ -2,40 +2,6 @@ import * as cacheUtils from '../src/cache-utils'
 import * as path from 'path'
 
 describe('cacheUtils-utils', () => {
-    describe('can hash', () => {
-        it('a directory', async () => {
-            const hash = await cacheUtils.hashFiles(
-                path.resolve('__tests__/data/crypto-utils-test/gradle')
-            )
-            expect(hash).toBe(
-                process.platform === 'win32'
-                    ? '3364336e94e746ce65a31748a6371b7efd7d499e18ad605c74c91cde0edc0a44'
-                    : '63b9f14f65d014e585099c9c274b9dcbddf5cfd1a8978e5a24efb89ff9304348'
-            )
-        })
-        it('a directory with a glob', async () => {
-            const hash = await cacheUtils.hashFiles(
-                path.resolve('__tests__/data/crypto-utils-test/'),
-                ['gradle/**']
-            )
-            expect(hash).toBe(
-                process.platform === 'win32'
-                    ? '3364336e94e746ce65a31748a6371b7efd7d499e18ad605c74c91cde0edc0a44'
-                    : '63b9f14f65d014e585099c9c274b9dcbddf5cfd1a8978e5a24efb89ff9304348'
-            )
-        })
-        it('a directory with globs', async () => {
-            const hash = await cacheUtils.hashFiles(
-                path.resolve('__tests__/data/crypto-utils-test/'),
-                ['**/*.gradle', 'gradle/**']
-            )
-            expect(hash).toBe(
-                process.platform === 'win32'
-                    ? 'd9b66fded38f79f601ce745d64ed726a8df8c0b242b02bcd2c1d331f54742ad6'
-                    : 'f42cd10636f09799f4e01cc84e7ae906cc1d9140f1446f8dcd054d19cbc44c2b'
-            )
-        })
-    })
     describe('can truncate args', () => {
         test('handles zero-length string', () => {
             expect(cacheUtils.truncateArgs('')).toBe('')
--- src/cache-utils.ts
+++ src/cache-utils.ts
@@ -1,17 +1,3 @@
-import * as path from 'path'
-import * as glob from '@actions/glob'
-
-export async function hashFiles(
-    baseDir: string,
-    patterns: string[] = ['**'],
-    followSymbolicLinks = false
-): Promise<string | null> {
-    const combinedPatterns = patterns
-        .map(pattern => `${baseDir}${path.sep}${pattern}`)
-        .join('\n')
-    return glob.hashFiles(combinedPatterns, {followSymbolicLinks})
-}
-
 export function truncateArgs(args: string): string {
     return args.trim().replace(/\s+/g, ' ').substr(0, 400)
 }
