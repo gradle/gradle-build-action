0ecbac99f3d278cb5a243f8dbed0834b23ded246
Daz DeBoer <daz@gradle.com>
2021-09-03 11:25:55 -0600

    Group log messsages for restore/save cache

--- src/caches.ts
+++ src/caches.ts
@@ -5,15 +5,23 @@ import * as core from '@actions/core'
 const BUILD_ROOT_DIR = 'BUILD_ROOT_DIR'
 
 export async function restore(buildRootDirectory: string): Promise<void> {
-    core.saveState(BUILD_ROOT_DIR, buildRootDirectory)
-
+    core.startGroup('Restore Gradle User Home from cache')
     await cacheGradleUserHome.restore()
+    core.endGroup()
+
+    core.startGroup('Restore project .gradle directory from cache')
+    core.saveState(BUILD_ROOT_DIR, buildRootDirectory)
     await cacheProjectDotGradle.restore(buildRootDirectory)
+    core.endGroup()
 }
 
 export async function save(): Promise<void> {
-    const buildRootDirectory = core.getState(BUILD_ROOT_DIR)
-
+    core.startGroup('Cache Gradle User Home')
     await cacheGradleUserHome.save()
+    core.endGroup()
+
+    core.startGroup('Cache project .gradle directory')
+    const buildRootDirectory = core.getState(BUILD_ROOT_DIR)
     await cacheProjectDotGradle.save(buildRootDirectory)
+    core.endGroup()
 }
