996094e8e808208e5738e8413b3f55d24d1c1eb7
Daz DeBoer <daz@gradle.com>
2021-11-15 09:21:55 -0700

    Avoid failing job on any failure in post-action
    Failures to store cache entries should not fail the action or the Job.
    This fix attempts to catch and log any unexpected errors that occur when
    saving cache entries.
    
    Fixes: #119
    Fixes: #120

--- src/post.ts
+++ src/post.ts
@@ -1,15 +1,24 @@
 import * as core from '@actions/core'
 import * as caches from './caches'
 
+// Catch and log any unhandled exceptions.  These exceptions can leak out of the uploadChunk method in
+// @actions/toolkit when a failed upload closes the file descriptor causing any in-process reads to
+// throw an uncaught exception.  Instead of failing this action, just warn.
+process.on('uncaughtException', e => handleFailure(e))
+
 // Invoked by GitHub Actions
 export async function run(): Promise<void> {
     try {
         await caches.save()
     } catch (error) {
-        core.setFailed(String(error))
-        if (error instanceof Error && error.stack) {
-            core.info(error.stack)
-        }
+        handleFailure(error)
+    }
+}
+
+function handleFailure(error: unknown): void {
+    core.warning(`Unhandled error saving cache - job will continue: ${error}`)
+    if (error instanceof Error && error.stack) {
+        core.info(error.stack)
     }
 }
 
