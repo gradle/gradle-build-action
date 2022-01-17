50ca2bca83dd56fdca775f2c611b21938f196acd
Daz DeBoer <daz@gradle.com>
2022-01-17 13:50:55 -0700

    Improve messages for cache-disabled scenario

--- src/caches.ts
+++ src/caches.ts
@@ -8,7 +8,7 @@ const GRADLE_USER_HOME = 'GRADLE_USER_HOME'
 const CACHE_LISTENER = 'CACHE_LISTENER'
 
 export async function restore(gradleUserHome: string): Promise<void> {
-    // Only restore cache on first action step in workflow.
+    // Bypass restore cache on all but first action step in workflow.
     if (process.env[CACHE_RESTORED_VAR]) {
         core.info('Cache only restored on first action step.')
         return
@@ -17,17 +17,15 @@ export async function restore(gradleUserHome: string): Promise<void> {
 
     // Initialize the Gradle User Home even when caching is disabled.
     const gradleStateCache = new GradleStateCache(gradleUserHome)
-    if (gradleStateCache.cacheOutputExists()) {
-        core.info('Gradle User Home already exists: will not restore from cache.')
-        // We still add init-scripts for build-scan capture
-        gradleStateCache.init()
-        return
-    }
-
     gradleStateCache.init()
 
     if (isCacheDisabled()) {
         core.info('Cache is disabled: will not restore state from previous builds.')
+        return
+    }
+
+    if (gradleStateCache.cacheOutputExists()) {
+        core.info('Gradle User Home already exists: will not restore from cache.')
         return
     }
 
@@ -72,7 +70,7 @@ function shouldSaveCaches(): boolean {
     }
 
     if (!core.getState(CACHE_RESTORED_VAR)) {
-        core.info('Cache will only be saved in final post-action step.')
+        core.info('Cache will not be saved: not restored in main action step.')
         return false
     }
 
