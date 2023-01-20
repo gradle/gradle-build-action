bff802913eb680d8868367cfd80d8f88c9fcdf94
Daz DeBoer <daz@gradle.com>
2023-01-19 18:09:32 -0700

    Bump JUnit

--- .github/workflow-samples/kotlin-dsl/build.gradle.kts
+++ .github/workflow-samples/kotlin-dsl/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     api("org.apache.commons:commons-math3:3.6.1")
     implementation("com.google.guava:guava:31.1-jre")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
 
 tasks.test {
