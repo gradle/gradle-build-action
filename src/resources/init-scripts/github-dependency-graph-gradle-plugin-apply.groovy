cef72ff9e46d27435883d90b1cbd77b83aa840b9
daz <daz@gradle.com>
2023-07-10 07:16:49 -0600

    Use latest github-dependency-graph-gradle-plugin

--- src/resources/init-scripts/github-dependency-graph-gradle-plugin-apply.groovy
+++ src/resources/init-scripts/github-dependency-graph-gradle-plugin-apply.groovy
@@ -3,7 +3,7 @@ buildscript {
     maven { url "https://plugins.gradle.org/m2/" }
   }
   dependencies {
-    classpath "org.gradle:github-dependency-graph-gradle-plugin:0.0.3"
+    classpath "org.gradle:github-dependency-graph-gradle-plugin:0.0.5"
   }
 }
 apply plugin: org.gradle.github.GitHubDependencyGraphPlugin
