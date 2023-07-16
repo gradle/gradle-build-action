295951f0545a4ab2a4e0cae12af880b0efad5f1e
daz <daz@gradle.com>
2023-07-15 22:40:08 -0600

    Fix finding gradlew.bat on windows

--- src/gradlew.ts
+++ src/gradlew.ts
@@ -4,7 +4,7 @@ import fs from 'fs'
 const IS_WINDOWS = process.platform === 'win32'
 
 export function wrapperScriptFilename(): string {
-    return IS_WINDOWS ? 'gradlew.bat' : './gradlew'
+    return IS_WINDOWS ? './gradlew.bat' : './gradlew'
 }
 
 export function installScriptFilename(): string {
