915a66c096a03101667f9df2e56c9efef558b165
daz <daz@gradle.com>
2023-07-17 15:46:14 -0600

    Bump dependency-graph version number

--- src/resources/init-scripts/github-dependency-graph-gradle-plugin-apply.groovy
+++ src/resources/init-scripts/github-dependency-graph-gradle-plugin-apply.groovy
@@ -3,7 +3,7 @@ buildscript {
     maven { url "https://plugins.gradle.org/m2/" }
   }
   dependencies {
-    classpath "org.gradle:github-dependency-graph-gradle-plugin:0.0.7"
+    classpath "org.gradle:github-dependency-graph-gradle-plugin:0.1.0"
   }
 }
 apply plugin: org.gradle.github.GitHubDependencyGraphPlugin
