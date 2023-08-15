43f8f93391772e71abc87da3d57acc475bd9ac79
daz <daz@gradle.com>
2023-08-14 19:55:05 -0600

    Update to GE plugin 3.14.1

--- test/init-scripts/src/test/groovy/com/gradle/gradlebuildaction/BaseInitScriptTest.groovy
+++ test/init-scripts/src/test/groovy/com/gradle/gradlebuildaction/BaseInitScriptTest.groovy
@@ -139,7 +139,7 @@ class BaseInitScriptTest extends Specification {
         } else {
             """
               plugins {
-                id 'com.gradle.enterprise' version '3.14'
+                id 'com.gradle.enterprise' version '3.14.1'
               }
               gradleEnterprise {
                 server = '$mockScansServer.address'
@@ -165,7 +165,7 @@ class BaseInitScriptTest extends Specification {
         } else if (gradleVersion < GradleVersion.version('6.0')) {
             """
               plugins {
-                id 'com.gradle.build-scan' version '3.14'
+                id 'com.gradle.build-scan' version '3.14.1'
               }
               gradleEnterprise {
                 server = '$mockScansServer.address'
