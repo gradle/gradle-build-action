556e86f0cc46583ccf813e5bb9c974da4144c87c
daz <daz@gradle.com>
2023-09-25 21:17:05 -0600

    Update to dep-graph plugin 0.4.0

--- test/init-scripts/src/test/groovy/com/gradle/gradlebuildaction/TestDependencyGraph.groovy
+++ test/init-scripts/src/test/groovy/com/gradle/gradlebuildaction/TestDependencyGraph.groovy
@@ -110,11 +110,11 @@ class TestDependencyGraph extends BaseInitScriptTest {
     def getEnvVars() {
         return [
             GITHUB_DEPENDENCY_GRAPH_ENABLED: "true",
-            GITHUB_JOB_CORRELATOR: "CORRELATOR",
-            GITHUB_JOB_ID: "1",
-            GITHUB_REF: "main",
-            GITHUB_SHA: "123456",
-            GITHUB_WORKSPACE: testProjectDir.absolutePath,
+            GITHUB_DEPENDENCY_GRAPH_JOB_CORRELATOR: "CORRELATOR",
+            GITHUB_DEPENDENCY_GRAPH_JOB_ID: "1",
+            GITHUB_DEPENDENCY_GRAPH_REF: "main",
+            GITHUB_DEPENDENCY_GRAPH_SHA: "123456",
+            GITHUB_DEPENDENCY_GRAPH_WORKSPACE: testProjectDir.absolutePath,
             DEPENDENCY_GRAPH_REPORT_DIR: reportsDir.absolutePath,
             GITHUB_OUTPUT: gitHubOutputFile.absolutePath
         ]
