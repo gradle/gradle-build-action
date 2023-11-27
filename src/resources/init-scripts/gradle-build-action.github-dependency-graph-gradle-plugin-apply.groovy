87a9a15658c426a54dd469d4fc7dc1a73ca9d4a6
daz <daz@gradle.com>
2023-11-27 17:45:04 +1000

    Use 1.0.0 release of dependency graph plugin

--- src/resources/init-scripts/gradle-build-action.github-dependency-graph-gradle-plugin-apply.groovy
+++ src/resources/init-scripts/gradle-build-action.github-dependency-graph-gradle-plugin-apply.groovy
@@ -3,7 +3,7 @@ buildscript {
     maven { url "https://plugins.gradle.org/m2/" }
   }
   dependencies {
-    classpath "org.gradle:github-dependency-graph-gradle-plugin:1.0.0-rc-1"
+    classpath "org.gradle:github-dependency-graph-gradle-plugin:1.0.0"
   }
 }
 apply plugin: org.gradle.github.GitHubDependencyGraphPlugin
